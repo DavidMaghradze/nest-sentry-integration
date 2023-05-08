ec35f695d1645cc5e43b0dcd4ac123223bb779ea
DavidMaghradze <davidmaghradze07@gmail.com>
2023-05-08 10:05:30 +0400

    add release, tranceSampleRate

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module';
 import * as Sentry from '@sentry/node';
 
 async function bootstrap() {
-  const app = await Nest
+  const app = await NestFactory.create(AppModule);
   Sentry.init({
     dsn: 'https://20e4886a85b4441890874ed34c3891fc@o4505132704595968.ingest.sentry.io/4505132706955264',
+    release: 'backend-sentry-github@v0.1.0',
+    tracesSampleRate: 1.0,
   });
-  
+
   await app.listen(3000);
 }
 bootstrap();
