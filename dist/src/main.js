dff299b8643b9ec47735632219cebeb9fb27f187
DavidMaghradze <davidmaghradze07@gmail.com>
2023-05-08 10:15:57 +0400

    test again

--- dist/src/main.js
+++ dist/src/main.js
@@ -3,10 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const Sentry = require("@sentry/node");
+const integrations_1 = require("@sentry/integrations");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     Sentry.init({
         dsn: 'https://20e4886a85b4441890874ed34c3891fc@o4505132704595968.ingest.sentry.io/4505132706955264',
+        integrations: [new integrations_1.ReportingObserver()],
+        tracesSampleRate: 1.0,
     });
     await app.listen(3000);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as Sentry from '@sentry/node';
+import { ReportingObserver as ReportingObserverIntegration } from '@sentry/integrations';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   Sentry.init({
     dsn: 'https://20e4886a85b4441890874ed34c3891fc@o4505132704595968.ingest.sentry.io/4505132706955264',
-    release: 'backend-sentry-github@v0.1.0',
+    integrations: [new ReportingObserverIntegration()],
     tracesSampleRate: 1.0,
   });
 
