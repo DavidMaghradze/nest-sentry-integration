074aa090e4edf980606ecc0a1eb4570a10f9183a
DavidMaghradze <davidmaghradze07@gmail.com>
2023-05-08 09:44:17 +0400

    test

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import * as Sentry from '@sentry/node';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await Nest
   Sentry.init({
     dsn: 'https://20e4886a85b4441890874ed34c3891fc@o4505132704595968.ingest.sentry.io/4505132706955264',
   });
