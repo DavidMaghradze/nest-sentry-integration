705340a6bd2ae79e3ff1f7bd499fd1067dc528b2
DavidMaghradze <davidmaghradze07@gmail.com>
2023-05-08 10:19:29 +0400

    add requesHandler

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   Sentry.init({
     dsn: 'https://20e4886a85b4441890874ed34c3891fc@o4505132704595968.ingest.sentry.io/4505132706955264',
-    integrations: [new ReportingObserverIntegration()],
+    release: 'test-project@1.0.0',
     tracesSampleRate: 1.0,
   });
 
+  app.use(Sentry.Handlers.requestHandler());
   await app.listen(3000);
 }
 bootstrap();
